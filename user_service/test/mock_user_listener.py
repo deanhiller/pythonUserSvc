c18e62338ef4c620a8d966eee409dbf24c37ec2e
Dean <dean@Deans-MacBook-Pro.local>
2022-03-17 20:12:01 -0600

    fix the git merge comments from the zip file...lol

--- user_service/test/mock_user_listener.py
+++ user_service/test/mock_user_listener.py
@@ -1,11 +1,6 @@
 from typing import List
 
-<<<<<<< HEAD:user_service/test/mock_user_listener.py
 from user_service.abstract_user import UserListener, User
-=======
-from user_service.src import UserListener, User
->>>>>>> b2aebacabf109b4538dc93c30f260b765902ac00:test/mock_user_listener.py
-
 
 class MockUserListener(UserListener):
 
